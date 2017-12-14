7821ec685bc06809a75b968cd9221e1cf84b0164
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-14 20:11:20 +0100

    Updated the routes authorizations

--- Client/App.vue
+++ Client/App.vue
@@ -1,50 +1,11 @@
 <template>
 <div id="app">
-  
   <router-view></router-view>
 </div>
 </template>
 
 <script>
-
-import gql from 'graphql-tag'
-import VueApollo from 'vue-apollo'
-import {apolloClient} from './apollo'
-import router from './router.js'
-import {mapMutations} from 'vuex'
-import {LOGGED_IN_USER} from './constants/loggedInUser.gql'
-
 export default {
   name: 'app',
-  methods: {
-    ...mapMutations([
-      'currentUserId'
-    ]),
-    isUserLoggedIn() {
-      this.$apollo.query({
-        query: LOGGED_IN_USER,
-      }).then(data => {  
-        if(this.loggedIn(data) === null){
-          this.$router.push({name:'login'});
-        } else {
-          this.$store.commit('currentUserId', data.data.loggedInUser.id);
-        }
-        if (this.loggedIn(data) !== null && this.$route.path === '/') {
-          this.$router.push({name:'signup'});
-        }
-      }).catch(error => {
-        console.error(error)
-      });
-    },
-    loggedIn(data){
-      return data.data.loggedInUser && data.data.loggedInUser.id !== ''
-    }
-  },
-  beforeCreated() {
-   this.isUserLoggedIn();
-  },
-  updated() {
-   this.isUserLoggedIn();
-  }
 }
 </script>
--- Client/router.js
+++ Client/router.js
@@ -5,6 +5,10 @@ import LoginForm from './components/login/LoginForm.vue'
 import SignupForm from './components/login/SignupForm.vue'
 import Dashboard from './components/main/Dashboard.vue'
 
+import {store} from './store'
+import {LOGGED_IN_USER} from './constants/loggedInUser.gql'
+import {apolloClient} from './apollo'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -13,6 +17,43 @@ const routes = [
     {name: "dashboard", path: "/dashboard", component: Dashboard}
 ]
 
-const router = new VueRouter({routes})
+const router = new VueRouter({mode:'history',routes})
+
+
+function loggedIn(data){
+    return data.data.loggedInUser && data.data.loggedInUser.id !== ''
+}
+
+router.beforeEach((to, from, next) => {
+    apolloClient.query({
+        query: LOGGED_IN_USER,
+      }).then(data => {  
+        // if user IS NOT logged in and access a resource othen than login
+        // goes back to login
+        if(loggedIn(data) === null && to.name !== 'login'){
+          next({name:'login'});
+
+        // if user is logged in and access a resource other than login
+        // stores currentUserId in store
+        // and goes to the resource requested
+        } else if (loggedIn(data) !== null) {
+          store.state.currentUserId = data.data.loggedInUser.id;
+          next();
+
+          // if user IS NOT logged in and access login
+          // goes to the resrource
+        } else {
+            next();
+        }
+
+        // if user IS logged in but access login
+        // goes back to dashboard
+        if (loggedIn(data) !== null && to.name === 'login') {
+          next({name:'dashboard'});
+        }
+      }).catch(error => {
+        console.error(error)
+      });
+})
 
 export default router;
