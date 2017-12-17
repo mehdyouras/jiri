45158bb8834b3177823ca6a5b29b3edd962266b6
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-17 17:36:04 +0100

    Added current user name to header

--- Client/components/sidebar/parts/AppHeader.vue
+++ Client/components/sidebar/parts/AppHeader.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
       <h1>Jiri</h1>
+      <p>{{currentUser.name}}</p>
       <clock></clock>
   </div>
 </template>
 <script>
 import Clock from './Clock.vue'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'AppHeader',
   components: {
       Clock,
   },
+  computed: {
+    ...mapGetters([
+      'currentUser'
+    ])
+  }
 }
 </script>
