1ec3f348b30efcaf11e6e29f4327a35256c6e11a
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-17 18:44:27 +0100

    Added userType to dashboard

--- Client/components/main/Dashboard.vue
+++ Client/components/main/Dashboard.vue
@@ -1,15 +1,31 @@
 <template>
   <div>
       <Sidebar></Sidebar>
+      <div>
+        <p>Vous êtes connecté en tant {{userType}}</p>
+      </div>
   </div>
 </template>
 <script>
 import Sidebar from '../sidebar/Sidebar.vue'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'dashboard',
   components: {
     Sidebar,
-  }
+  },
+  computed: {
+    ...mapGetters([
+      'currentUser'
+    ]),
+    userType() {
+      if(this.currentUser.isAdmin) {
+        return 'qu\'administrateur'
+      } else {
+        return 'que ' + this.currentUser.name
+      }
+    }
+  },
 }
 </script>
