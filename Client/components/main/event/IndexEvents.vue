92763c65000db96ce522b02c35f6da8bdb760b4e
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-25 20:41:35 +0100

    Added IndexEvents initials files

---
+++ Client/components/main/event/IndexEvents.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h2>Vue d'ensemble des événements</h2>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexEvents',
+}
+</script>
+
+<style>
+
+</style>
---
+++ Client/components/main/event/indexParts/SingleEvent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h3>{{event.course}} {{event.year}} - {{event.session}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SingleEvent',
+}
+</script>
+
+<style>
+
+</style>
