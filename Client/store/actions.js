23c5e79a40b0a2d70ddc08838fc552a612a286d1
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-26 21:31:30 +0100

    Removed useless vuex actions

--- Client/store/actions.js
+++ Client/store/actions.js
@@ -5,39 +5,5 @@ import {ALL_PROJECTS} from '../constants/allProjects.gql'
 import {apolloClient} from '../apollo'
 
 export const actions = {
-    setCurrentUser({state, commit, mutations}) {
-        let id = state.currentUserId;
-        apolloClient.query(
-            {
-                query: USER,
-                variables: {
-                  id
-                }
-            }
-        ).then(data => {
-          commit('currentUser', data.data.User)
-        }).catch(e => {
-          console.log(e)
-        })
-    },
 
-    setAllDetails({state, commit, mutations}) {
-        apolloClient.query({
-            query: ALL_STUDENTS_USERS_PROJECTS,
-            }).then((data) => {
-                    commit('setAllDetails', data.data)
-                    console.log(data.data.allProjects)
-                }
-            )
-    },
-
-    setAllProjects({state,commit}) {
-        apolloClient.query({
-            query: ALL_PROJECTS,
-            }).then((data) => {
-                    commit('setAllProjects', data.data)
-                    console.log(data.data)
-                }
-            )
-    },
 }
