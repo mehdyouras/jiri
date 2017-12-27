73cba723348763363481d110313d5300c1a92c16
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-27 23:59:55 +0100

    Added createScore gql mutation

--- Client/constants/index.js
+++ Client/constants/index.js
@@ -12,6 +12,7 @@ export { SIGNUP_USER } from './signupUser.gql';
 export { AUTHENTICATE_USER } from './authenticateUser.gql'
 export { CREATE_EVENT } from './createEvent.gql';
 export { CREATE_IMPLEMENTATION } from './createImplementation.gql';
+export { CREATE_SCORE } from './createScore.gql';
 
 export { DELETE_PROJECT } from './deleteProject.gql';
 export { DELETE_USER } from './deleteUser.gql';
