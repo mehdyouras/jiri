de06837bbe8a62729905c50256736d0e04b811a6
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-27 18:17:34 +0100

    Partitioned indexStudents

---
+++ Client/components/common/indexes/Students.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+      <ol>
+          <li @click="studentClicked(student.id)" v-for="student in students" :key="student.id">
+              <span>{{student.name}}</span>
+              <span>{{student.email}}</span>
+          </li>
+      </ol>
+  </div>
+</template>
+
+<script>
+import {ALL_STUDENTS} from '../../../constants'
+
+export default {
+    name: 'IndexStudents',
+    data() {
+        return {
+            students: {},
+        }
+    },
+    apollo: {
+        students: {
+            query: ALL_STUDENTS,
+            update(data) {
+                return data.allStudents
+            }
+        }
+    },
+    methods: {
+        studentClicked(id) {
+            this.$emit('studentClicked', id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- Client/components/main/student/IndexStudents.vue
+++ Client/components/main/student/IndexStudents.vue
@@ -1,32 +1,18 @@
 <template>
   <div>
       <h2>Vue d'ensemble des étudiants</h2>
-      <ol>
-          <li @click="goToAddMeeting(student.id)" v-for="student in students" :key="student.id">
-              <span>{{student.name}}</span>
-              <span>{{student.email}}</span>
-          </li>
-      </ol>
+      <Students @studentClicked="goToAddMeeting"></Students>
   </div>
 </template>
 
 <script>
 import {ALL_STUDENTS} from '../../../constants'
+import Students from '../../common/indexes/Students'
 
 export default {
     name: 'IndexStudents',
-    data() {
-        return {
-            students: {},
-        }
-    },
-    apollo: {
-        students: {
-            query: ALL_STUDENTS,
-            update(data) {
-                return data.allStudents
-            }
-        }
+    components: {
+        Students,
     },
     methods: {
         goToAddMeeting(id) {
