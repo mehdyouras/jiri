5c4e56addfbd6f713c868479e49a97da4a120658
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-28 00:00:24 +0100

    Added first step to chooseImplementations handling

--- Client/components/main/meeting/ChooseImplementations.vue
+++ Client/components/main/meeting/ChooseImplementations.vue
@@ -2,53 +2,92 @@
   <div>
       <h2>Organiser une rencontre avec un étudiant</h2>
       <h3>{{student.name}}</h3>
-      <ol>
-          <li @click="showScoreForm(project)" v-for="project in student.event.weights" :key="project.id">
-              {{project.project.name}}
-          </li>
-      </ol>
-      <ol>
-          <li v-if="isAdding">
+        <Spinner v-if="isLoading"></Spinner>
+        <template v-else>   
+            <ol>
+                <li @click="showScoreForm(implementation)" v-for="implementation in student.implementations" :key="implementation.id">
+                    {{implementation.project.name}}
+                </li>
+            </ol>
+            <ol>
+                <li v-if="isAdding">
+                    <h4>{{implementationAdded.project.name}}</h4>
+                    <label for="comment">Description</label>
+                    <textarea v-model="comment" name="comment" id="comment" cols="30" rows="10"></textarea>
 
-          </li>
-      </ol>
+                    <label for="score">Note</label>
+                    <input v-model.number="score" type="number" name="score" id="score">
+
+                    <button @click="addScore">Noter</button>
+                </li>
+                <li v-for="score in student.scores" :key="score.id">
+                    <h4>{{score.implementation.project.name}}</h4>
+                    <p>{{score.comment}}</p>
+                    <p>Note : {{score.score}}</p>
+                </li>
+            </ol>
+        </template>
   </div>
 </template>
 
 <script>
 import {STUDENT} from '../../../constants'
+import {Bus} from '../../../Bus'
+import Spinner from '../../common/Spinner'
 
 export default {
     name: 'ChooseImplementationsForMeeting',
     data() {
         return {
+            isLoading: 0,
             student: {},
             isAdding: false,
+            implementationAdded: {},
+            comment: '',
+            score: '',
         }
+    },
+    components: {
+        Spinner
     },
     methods: {
-
+        showScoreForm(implementation) {
+            this.isAdding = true;
+            this.implementationAdded = implementation;
+        },
+        addScore() {
+            let payload = {
+                studentId: this.student.id,
+                implementationId: this.implementationAdded.id,
+                comment: this.comment,
+                score: this.score,
+            }
+            Bus.$emit('createScore', payload)
+            this.isAdding = false;
+            this.comment, this.score = '';
+        }
     },
-    created() {
-        // Checks if the studentId in route exists
-        if(this.$route.params.studentId) {
-            this.$apollo.query({
-                query: STUDENT,
-                variables: {
+    apollo: {
+        student: {
+            query: STUDENT,
+            variables() {
+                return {
                     id: this.$route.params.studentId
                 }
-            }).then(data => {
-                if(data.data.Student === null) {
+            },
+            loadingKey: 'isLoading',
+            error(error) {
+                this.$router.push({name: 'addMeeting'})
+            },
+            update(data) {
+                if(!data.Student) {
                     this.$router.push({name: 'addMeeting'})
                 } else {
-                    console.log(data.data.Student)
-                    this.student = data.data.Student;
+                    return data.Student
                 }
-            });
-        } else {
-            this.$router.push({name: 'addMeeting'})
+            }
         }
-    }
+    },
 }
 </script>
 
