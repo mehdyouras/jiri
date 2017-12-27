b1f48aa9ed3da7c4ff20ccfd03d8f660b46e573c
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-27 16:45:12 +0100

    Added all projects associated to student linked event

--- Client/components/main/meeting/ChooseImplementations.vue
+++ Client/components/main/meeting/ChooseImplementations.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
       <h2>Organiser une rencontre avec un étudiant</h2>
-      <p>implementations</p>
+      <h3>{{student.name}}</h3>
+      <ol>
+          <li v-for="project in student.event.weights" :key="project.id">
+              {{project.project.name}}
+          </li>
+      </ol>
   </div>
 </template>
 
@@ -10,29 +15,30 @@ import {STUDENT} from '../../../constants'
 
 export default {
     name: 'ChooseImplementations',
-    methods: {
-        redirect() {
-            // Choose the appropriate component depending on studentId in route
-            if(this.$route.params.studentId) {
-                this.$apollo.query({
-                    query: STUDENT,
-                    variables: {
-                        id: this.$route.params.studentId
-                    }
-                }).then(data => {
-                    if(data.data.Student === null) {
-                        this.$router.push({name: 'addMeeting'})
-                    } else {
-                        console.log('Good route')
-                    }
-                });
-            } else {
-                this.$router.push({name: 'addMeeting'})
-            }
+    data() {
+        return {
+            student: {},
         }
     },
     created() {
-        this.redirect();
+        // Checks if the studentId in route exists
+        if(this.$route.params.studentId) {
+            this.$apollo.query({
+                query: STUDENT,
+                variables: {
+                    id: this.$route.params.studentId
+                }
+            }).then(data => {
+                if(data.data.Student === null) {
+                    this.$router.push({name: 'addMeeting'})
+                } else {
+                    console.log(data.data.Student)
+                    this.student = data.data.Student;
+                }
+            });
+        } else {
+            this.$router.push({name: 'addMeeting'})
+        }
     }
 }
 </script>
