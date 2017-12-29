a6bc6a7f333e7f880579f28ae2ccf0b3efe4c080
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-29 01:23:56 +0100

    Added vuex store binding with input

--- Client/components/main/student/editParts/SingleProjectForm.vue
+++ Client/components/main/student/editParts/SingleProjectForm.vue
@@ -5,10 +5,10 @@
     
     <div v-show="isChecked">
       <label :for="project.id + 'a'">URL repo Github</label>
-      <input v-model="urlRepo" type="text" :name="project.id + 'a'" :id="project.id + 'a'">
+      <input @input="save" v-model="urlRepo" type="text" :name="project.id + 'a'" :id="project.id + 'a'">
 
       <label :for="project.id + 'b'">URL du site</label>
-      <input v-model="urlProject" type="text" :name="project.id + 'b'" :id="project.id + 'b'">
+      <input @input="save" v-model="urlProject" type="text" :name="project.id + 'b'" :id="project.id + 'b'">
     </div>
 
   </div>
@@ -21,7 +21,6 @@ export default {
     name: 'SingleProjectForm',
     data() {
       return {
-        implementationIndex: '',
         isChecked: false,
         urlRepo: '',
         urlProject: '',
@@ -31,9 +30,20 @@ export default {
       'project',
       'student'
     ],
+    methods: {
+      save() {
+        let payload = {
+          studentId: this.$route.params.studentId,
+          projectId: this.project.id,
+          urlRepo: this.urlRepo,
+          urlProject: this.urlProject,
+        };
+        this.$store.commit('currentAddedImplementations', payload)
+      }
+    },
     created() {
-      this.implementationIndex = _.findIndex(this.student.implementations, {project: {id: this.project.id}});
-        if(this.implementationIndex !== -1) {
+      let implementationIndex = _.findIndex(this.student.implementations, {project: {id: this.project.id}});
+        if(implementationIndex !== -1) {
           this.isChecked = true;
           this.urlRepo = this.student.implementations[implementationIndex].urlRepo;
           this.urlProject = this.student.implementations[implementationIndex].urlProject;
