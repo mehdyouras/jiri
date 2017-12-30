da6110d599d134373b2ad8c35283ee8e0bdf7135
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-30 01:41:51 +0100

    Added a spinner ton students index

--- Client/components/common/indexes/Students.vue
+++ Client/components/common/indexes/Students.vue
@@ -1,22 +1,34 @@
 <template>
   <div>
-      <ol>
-          <li @click="studentClicked(student.id)" v-for="student in students" :key="student.id">
-              <span>{{student.name}}</span>
-              <span>{{student.email}}</span>
-          </li>
-      </ol>
+      <Spinner v-if="isLoading"></Spinner>
+      <template v-else>
+        <router-link :to="{name: 'addEvent'}">Ajouter un étudiant</router-link>
+        <template v-if="!students[0]">
+            <p>Il n'y a pas encore d'étudiant</p>
+        </template>
+        <ol v-else>
+            <li @click="studentClicked(student.id)" v-for="student in students" :key="student.id">
+                <span>{{student.name}}</span>
+                <span>{{student.email}}</span>
+            </li>
+        </ol>
+      </template>
   </div>
 </template>
 
 <script>
 import {ALL_STUDENTS} from '../../../constants'
+import Spinner from '../../common/Spinner'
 
 export default {
     name: 'IndexStudents',
+    components: {
+        Spinner
+    },
     data() {
         return {
             students: {},
+            isLoading: 0,
         }
     },
     apollo: {
@@ -24,7 +36,8 @@ export default {
             query: ALL_STUDENTS,
             update(data) {
                 return data.allStudents
-            }
+            },
+            loadingKey: 'isLoading',
         }
     },
     methods: {
--- Client/constants/index.js
+++ Client/constants/index.js
@@ -23,6 +23,7 @@ export { UPDATE_USER } from './updateUser.gql'
 export { UPDATE_USER_PASSWORD } from './updateUser.gql'
 export { UPDATE_EVENT } from './updateEvent.gql'
 export { UPDATE_IMPLEMENTATION } from './updateImplementation.gql'
+export { ADD_STUDENT_TO_EVENT } from './addStudentToEvent.gql'
 
 export { DELETE_PROJECT } from './deleteProject.gql';
 export { DELETE_USER } from './deleteUser.gql';
