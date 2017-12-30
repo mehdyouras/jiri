4db11d840eb6d1fa1908ba1cbb894234d6ad81b4
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-12-30 02:03:31 +0100

    Added way to not show already added implementation

--- Client/components/common/forms/ImplementationForm.vue
+++ Client/components/common/forms/ImplementationForm.vue
@@ -1,32 +1,36 @@
 <template>
   <div>
-        <label for="project">Nom du projet</label>
-        <select v-model="projectId" name="project" id="project">
-            <option disabled value="">Choisissez</option>
-            <option v-for="weight in student.event.weights" :key="weight.project.id" :value="weight.project.id">
-                {{weight.project.name}}
-            </option>
-        </select>
+        <template v-if="projectsToShow[0]">
+            <label for="project">Nom du projet</label>
+            <select v-model="projectId" name="project" id="project">
+                <option disabled value="">Choisissez</option>
+                <option v-for="weight in projectsToShow" :key="weight.project.id" :value="weight.project.id">
+                    {{weight.project.name}}
+                </option>
+            </select>
 
-        <label for="urlRepo">URL du repo Github</label>
-        <input v-model="urlRepo" type="text" name="urlRepo" id="urlRepo">
+            <label for="urlRepo">URL du repo Github</label>
+            <input v-model="urlRepo" type="text" name="urlRepo" id="urlRepo">
 
-        <label for="urlProject">URL du projet</label>
-        <input v-model="urlProject" type="text" name="urlProject" id="urlProject">
+            <label for="urlProject">URL du projet</label>
+            <input v-model="urlProject" type="text" name="urlProject" id="urlProject">
 
-        <button @click="addItem">Ajouter</button>
+            <button @click="addItem">Ajouter</button>
+        </template>
   </div>
 </template>
 
 <script>
 import {STUDENT} from '../../../constants'
 import {Bus} from '../../../Bus'
+import _ from 'lodash'
 
 export default {
     name: 'ImplementationForm',
     data() {
         return {
             student: {},
+            projectsToShow: {},
             urlRepo: '',
             urlProject: '',
             projectId: '',
@@ -53,6 +57,21 @@ export default {
             },
             loadingKey: 'isLoading',
             update(data) {
+                let projectsToHide = [];
+                data.Student.implementations.forEach(element => {
+                    projectsToHide.push(element.project.id)
+                });
+                
+                let projectsToShow = _.filter(data.Student.event.weights, weight => {
+                    return !!_.find(projectsToHide, weight.project.id)
+                }) 
+
+                if(!projectsToShow[0]) {
+                    this.$emit('dontShowAdd');
+                }
+
+                this.projectsToShow = projectsToShow;
+
                 return data.Student
             }
         }
