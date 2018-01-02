aa03ee261065ac4b2ad842a861ab7726cac439b7
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-02 01:00:12 +0100

    Added style

--- Client/components/login/LoginForm.vue
+++ Client/components/login/LoginForm.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>
-      <h1>Jiri</h1>
+  <section class="section columns is-medium column is-centered">
+    <div class="card column is-one-quarter">
+      <h1 class="title is-1 has-text-centered">Jiri</h1>
       <form method="post" action="#">
-        <label for="email-input">Adresse email</label>
-        <input v-model="email" type="text" id="email-input">
-        <label for="password-input">Mot de passe</label>
-        <input v-model="password" type="password" id="password-input">
+        <b-field :type="this.errors.has('email') ? 'is-danger' : null" label="Adresse email">
+          <b-input v-validate="'required|email'" v-model="email" type="email" name="email"></b-input>
+        </b-field>
+        <b-field :type="this.errors.has('password') ? 'is-danger' : null" label="Mot de passe">
+          <b-input v-validate="'required'" name="password" v-model="password" type="password"></b-input>
+        </b-field>
 
-        <input v-if="!isLoggingIn" @click.prevent="checkLogin" type="submit">
-        <Spinner v-else></Spinner>
+        <a @click.prevent="checkLogin" :class="['button', 'is-primary', {'is-loading': isLoggingIn}, 'mdi', 'mdi-login', 'mdi-18px']" href="#">
+          Se connecter
+        </a>
       </form>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -38,7 +43,12 @@ export default {
   methods: {
       checkLogin() {
         let {email, password} = this
-        Bus.$emit('authenticateUser', {email, password})
+        this.$validator.validateAll().then((result) => {
+                if (result) {
+                  Bus.$emit('authenticateUser', {email, password})
+                return;
+                }
+            })
       },
       logoutUser() {
         localStorage.removeItem('userToken')
