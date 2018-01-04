58340ae3ee356f77caa52bbd80979cf9a5e3d6fb
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-04 16:34:29 +0100

    Added bootstrap to StudenForm

--- Client/components/common/forms/StudentForm.vue
+++ Client/components/common/forms/StudentForm.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
+  <b-card class="col-md-5">
+    <b-form-group
+              label="Adresse email"
+              label-for="student-email"
+              :invalid-feedback="this.errors.first('student-email')"
+              :state="!this.errors.has('student-email')"
+            >
+              <b-form-input id="student-email" name="student-email" type="email" v-validate="'required|email'" v-model.trim="email" :state="!this.errors.has('student-email')"></b-form-input>
+    </b-form-group>
+<!--     
     <label for="student-email">Adresse email</label>
     <input v-validate="'required|email'" v-model="email" type="email" id="student-email">
-    <span v-show="this.errors.has('student-email')">{{this.errors.first('student-email')}}</span>
+    <span v-show="this.errors.has('student-email')">{{this.errors.first('student-email')}}</span> -->
 
-    <label for="student-name">Nom</label>
+    <b-form-group
+              label="Nom"
+              label-for="student-name"
+              :invalid-feedback="this.errors.first('student-name')"
+              :state="!this.errors.has('student-name')"
+            >
+              <b-form-input id="student-name" name="student-name" type="text" v-validate="'required|alpha_spaces'" v-model.trim="name" :state="!this.errors.has('student-name')"></b-form-input>
+    </b-form-group>
+
+    <!-- <label for="student-name">Nom</label>
     <input v-validate="'required|alpha_spaces'" v-model="name" type="text" id="student-name">
-    <span v-show="this.errors.has('student-name')">{{this.errors.first('student-name')}}</span>
+    <span v-show="this.errors.has('student-name')">{{this.errors.first('student-name')}}</span> -->
 
     <button @click.prevent="createStudent">Ajouter</button>
-  </div>
+  </b-card>
 </template>
 
 <script>
