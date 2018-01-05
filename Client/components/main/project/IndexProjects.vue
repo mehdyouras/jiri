0a027da898223099da939b4b6040e8f9f33df1e2
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-05 18:35:19 +0100

    Added modal to remove project

--- Client/components/main/project/IndexProjects.vue
+++ Client/components/main/project/IndexProjects.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-      <h2>Vue d'ensemble des projets</h2>
+  <b-card>
+      <h2 class="mb-3">Vue d'ensemble des projets</h2>
       <Spinner v-if="isLoading"></Spinner>
       <template v-else>
-        <b-button v-b-toggle.addForm variant="primary" class="mb-3" @click="addType">Ajouter un projet</b-button>
+        <b-button v-b-toggle.addForm variant="primary" class="mb-3">Ajouter un projet</b-button>
         <b-collapse id="addForm" class="">
             <b-card class="col-md-5">
                 <ProjectForm></ProjectForm>
@@ -12,19 +12,36 @@
         <template v-if="!projects[0]">
             <p>Il n'y a pas de projet.</p>
         </template>
-        <ol>
-            <li v-for="project in projects" :key="project.id">
-                <span>{{project.name}}</span>
+        <ol class="list-unstyled row mt-3">
+            <li class="col-md-4 col-lg-3" v-for="project in projects" :key="project.id">
+                <b-card no-body class="mb-3">
+                    <b-card-body class="card-text d-flex justify-content-between align-items-center">
+                        <div>
+                            <span class="d-block">
+                                {{project.name}}
+                            </span>
+                        </div>
+                        <b-dropdown right v-if="isAdmin" variant="light">
+                            <b-dropdown-item @click="editProject">Modifier</b-dropdown-item>
+                            <b-dropdown-item @click="openModal({id:project.id, name: project.name, type: 'project'})" class="text-danger">Supprimer</b-dropdown-item>
+                        </b-dropdown>
+                    </b-card-body>
+                </b-card>
             </li>
         </ol>
       </template>
-  </div>
+      <b-modal @ok="deleteProject" ref="delete" title="Confirmation" ok-title="Supprimer" ok-variant="danger" cancel-title="Annuler">
+          Êtes-vous sûr de vouloir <strong class="text-danger">supprimer</strong> le projet <strong>{{modal.name}}</strong> ?
+      </b-modal>
+  </b-card>
 </template>
 
 <script>
 import {ALL_PROJECTS} from '../../../constants'
 import ProjectForm from '../../common/forms/ProjectForm'
 import Spinner from '../../common/Spinner'
+import {mapGetters} from 'vuex'
+import {Bus} from '../../../Bus'
 
 export default {
     name: 'IndexUsers',
@@ -36,7 +53,17 @@ export default {
         return {
             projects: {},
             isLoading: 0,
+            modal: {
+                id: "",
+                name: "",
+                type: "project",
+            }
         }
+    },
+    computed: {
+        ...mapGetters([
+            'isAdmin'
+        ])
     },
     apollo: {
         projects: {
@@ -45,6 +72,18 @@ export default {
                 return data.allProjects
             },
             loadingKey: 'isLoading',
+        }
+    },
+    methods: {
+        editProject() {
+            
+        },
+        openModal(payload) {
+            this.modal = payload;
+            this.$refs.delete.show()
+        },
+        deleteProject() {
+            Bus.$emit('removeItem', this.modal);
         }
     }
 }
