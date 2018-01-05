9022d90eb9616a220cc5892b848f5c885f12a713
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-06 00:44:04 +0100

    Added push to editUser

--- Client/components/main/user/IndexUsers.vue
+++ Client/components/main/user/IndexUsers.vue
@@ -16,7 +16,7 @@
             <li class="col-md-4 col-lg-3" v-for="user in users" :key="user.id">
                 <b-card no-body show variant="secondary" class="mb-3">
                     <div class="card-text d-flex justify-content-between align-items-center">
-                        <div class="p-3 card-clickable">
+                        <div @click="editUser(user.id)" class="p-3 card-clickable">
                             <span class="d-block">
                                 {{user.name}}
                             </span>
