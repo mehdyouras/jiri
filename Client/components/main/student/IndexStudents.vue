fd144cb0aabb23c8a8098de295a6de526908df44
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-05 16:39:19 +0100

    Fixed dropdown clipping

--- Client/components/main/student/IndexStudents.vue
+++ Client/components/main/student/IndexStudents.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
       <h2>Vue d'ensemble des étudiants</h2>
-      <Students :editable="true" @studentClicked="goToAddMeeting"></Students>
+      <Students @deleteModal="openModal" :editable="true" @studentClicked="goToAddMeeting"></Students>
+      <b-modal @ok="removeEvent(modal.id)" ref="delete" title="Confirmation" ok-title="Supprimer" ok-variant="danger" cancel-title="Annuler">
+          Êtes-vous sûr de vouloir <strong class="text-danger">supprimer</strong> l'événement <strong>{{modal.name}}</strong> ?
+      </b-modal>
   </div>
 </template>
 
@@ -11,12 +14,31 @@ import Students from '../../common/indexes/Students'
 
 export default {
     name: 'IndexStudents',
+    data() {
+        return {
+            modal: {
+                id: "",
+                name: "",
+                type: "",
+            }
+        }
+    },
     components: {
         Students,
     },
     methods: {
         goToAddMeeting(id) {
             this.$router.push({name:'addImplementationsToMeeting', params: {studentId: id}})
+        },
+        openModal(payload) {
+            this.modal = payload;
+            this.$refs.delete.show()
+        },
+        deleteItem() {
+            Bus.$emit('deleteItem', this.modal);
+            this.modal.id = "";
+            this.modal.name = "";
+            this.modal.type = "";
         }
     }
 }
