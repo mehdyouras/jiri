2683bdba38b639eab41994ba0299c594fc0bfd72
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-11 09:31:03 +0100

    Renamed to js file

--- Client/store/actions.js
+++ Client/store/actions.js
@@ -1,7 +1,7 @@
-import {USER} from '../constants/user.gql'
-import {ALL_STUDENTS_USERS_PROJECTS} from '../constants/allStudentsUsersProjects.gql'
-import {CREATE_PROJECT} from '../constants/createProject.gql'
-import {ALL_PROJECTS} from '../constants/allProjects.gql'
+import {USER} from '../constants/user.js'
+import {ALL_STUDENTS_USERS_PROJECTS} from '../constants/allStudentsUsersProjects.js'
+import {CREATE_PROJECT} from '../constants/createProject.js'
+import {ALL_PROJECTS} from '../constants/allProjects.js'
 import {apolloClient} from '../apollo'
 
 export const actions = {
