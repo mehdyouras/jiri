9bacffe2298877f70486066b0e57d17d763cd0f6
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-11 08:19:34 +0100

    Fixed printEvent

--- Client/components/main/print/PrintEvent.vue
+++ Client/components/main/print/PrintEvent.vue
@@ -1,14 +1,30 @@
 <template>
     <section>
         <h2 class="mb-3">Rencontres entre jurys et étudiants</h2>
-        <ul v-for="(jury, index) in event.jurys" :key="jury.id">
-            <li>
-                {{jury.name}}
-                <ol>
-                    <li v-for="student in studentsToAssign()[index]" :key="student.id">{{student.name}}</li>
-                </ol>
-            </li>
-        </ul>
+        <section>
+            <h3>Jurys</h3>
+            <ul class="list-unstyled" v-for="(jury, index) in event.jurys" :key="jury.id">
+                <li>
+                    {{jury.name}}
+                    <ol class="list-unstyled mt-3">
+                        <li v-for="(student, i) in studentsToAssign()[index]" :key="student.id"><strong>{{time[i]}}</strong> {{student.name}}</li>
+                    </ol>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h3>Étudiants</h3>
+            <ul v-for="student in event.students" :key="student.id">
+                <li>
+                    {{student.name}}
+                    <ol>
+                        <template v-for="(jury, i) in studentJurys(student.id)" >
+                            <li v-if="jury[0]" :key="jury.id"><strong>{{time[i]}}</strong> {{event.jurys[i].name}}</li>
+                        </template>
+                    </ol>
+                </li>
+            </ul>
+        </section>
     </section>
 </template>
 
@@ -20,21 +36,15 @@ export default {
         return {
             eventId: this.$route.params.eventId,
             event: {},
+            allStudents: {},
+            time: ["8h45 - 9h30", "9h30 - 10h00", "10h00 - 11h00", "11h40 - 12h10", "13h50 - 14h50", "15h00 - 16h00", "16h10 - 17h15"],
         }
-    },
-    computed: {
-        // numberOfStudentsToAssign() {
-        //     let number = this.event.students.length < 7 ? this.event.students.length : Math.ceil(this.event.students.length / this.event.jurys.length);
-        //     return number
-        // },
-        allStudents() {
-            return _.filter(this.event.students, student => true)
-        },
     },
     methods: {
         studentsToAssign() {
             let allStudents = this.allStudents;
             let studentsToAssign = [];
+
             if(this.allStudents.length % 7 === 0) {
                 for (let index = 0; index < this.event.jurys.length; index++) {
                     let last = _.last(allStudents);
@@ -42,12 +52,27 @@ export default {
                     allStudents.unshift(last);
                     studentsToAssign.push(allStudents)
                 }
-                return studentsToAssign
+                return _.dropRight(studentsToAssign, studentsToAssign.length - this.event.jurys.length)
             }
             for (let index = 0; index < this.event.jurys.length; index++) {
                 studentsToAssign = _.concat(studentsToAssign, allStudents)
             }
-            return _.chunk(studentsToAssign, 7)
+
+            studentsToAssign = _.chunk(studentsToAssign, 7);
+
+            return _.dropRight(studentsToAssign, studentsToAssign.length - this.event.jurys.length)
+        },
+        studentJurys(id) {
+            let studentJurys = [];
+            let studentsToAssign = this.studentsToAssign();
+
+            studentsToAssign.forEach((jury, index) => {
+                studentJurys.push(_.filter(jury, (student) => {
+                    return student.id === id
+                }))
+            });
+            
+            return studentJurys
         }
     },
     apollo: {
@@ -59,6 +84,7 @@ export default {
                 }
             },
             update(data) {
+                this.allStudents = JSON.parse(JSON.stringify(data.Event.students));
                 return data.Event
             }
         }
